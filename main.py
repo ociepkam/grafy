ab4a4cf5eb3959a0ee850e61024f44b377109d2b
Jan <jastrz.jan@gmail.com>
2020-12-11 19:00:51 +0100

    kolejność kolumn, ID do wyników, usunięcie bloków, triale z podziałem na trening/exp,

--- main.py
+++ main.py
@@ -15,12 +15,14 @@ NAME = "{}_{}_{}".format(part_id, part_sex[:1], part_age)
 RAND = str(random.randint(100, 999))
 
 RESULTS = list()
-RESULTS.append(["ORDER", "NR", 'EXPERIMENT', "FEED",
+RESULTS.append(["ID", "SEX", "AGE", "DATE",
+                "ORDER", "NR", 'EXPERIMENT', "FEED",
+                "VA", "EA", "VB", "EB", "left", "right",
+                "NV", "NE", "Type", "Crossed_edges",
                 "LEFT_ANS", "RIGHT_ANS",
                 'LEFT_ACC', "RIGHT_ACC", "ACC",
-                "LEFT_RT", "RIGHT_RT", 'RT',
-                "VA", "EA", "VB", "EB", "left", "right", "NV", "NE", "Bidirectional", "Type",
-                "Crossed_edges", "Block", "A_to_B_relation"])
+                "LEFT_RT", "RIGHT_RT", 'RT'
+                ])
 
 
 @atexit.register
@@ -30,25 +32,27 @@ def save_beh():
         beh_writer.writerows(RESULTS)
 
 
-def prepare_result(i, info, answers, rt, acc, exp, A_to_B_relation):
-    return [i, info["NR"], exp, info["FEED"],
+def prepare_result(i, info, answers, rt, acc, exp):
+    return [part_id, part_sex, part_age, date,
+            i, info["NR"], exp, info["FEED"],
+            # "VA", "EA", "VB", "EB", "left", "right",
+            info["VA"], info["EA"], info["VB"], info["EB"], info["left"], info["right"],
+            # "NV", "NE", "Type", Crossed_edges
+            info["NV"], info["NE"], info["Type"], info["Crossed_edges"],
             # "LEFT_ANS", "RIGHT_ANS",
             answers["left"], answers["right"],
             # 'LEFT_ACC', "RIGHT_ACC", "ACC",
             acc["left"], acc["right"], acc["left"] and acc["right"],
             # "LEFT_RT", "RIGHT_RT", 'RT',
-            rt["left"], rt["right"], max(rt["left"], rt["right"]) if rt["left"] and rt["right"] else None,
-            # "VA", "EA", "VB", "EB", "left", "right",
-            info["VA"], info["EA"], info["VB"], info["EB"], info["left"], info["right"],
-            # "NV", "NE" "Bidirectional", "Type",
-            info["NV"], info["NE"], info["Bidirectional"], info["Type"],
-            info["Crossed_edges"], info["Block"], A_to_B_relation]
+            rt["left"], rt["right"], max(rt["left"], rt["right"]) if rt["left"] and rt["right"] else None]
 
 
 config = load_config()
-file_name = config['trials']
-data_train, data_exp = load_trials(file_name)
 
+data_train = load_trials(config['training_trials'])
+data_exp = load_trials(config['experiment_trials'])
+
+### DO WYWALENIA BLOKI:
 if config["random_trials_order"]:
     for block in data_exp:
         random.shuffle(block)
@@ -63,7 +67,7 @@ clock_image = visual.ImageStim(win=window, image=join('images', 'clock.png'), in
                                size=config['CLOCK_SIZE'], pos=config['CLOCK_POS'])
 
 mouse_info = visual.ImageStim(win=window, image=join('images', 'mouse_info.PNG'), interpolate=True,
-                               size=130, pos=[-60, -250])
+                              size=130, pos=[-60, -250])
 
 answers_colors = config["answers_colors"]
 
@@ -71,9 +75,6 @@ pos_feedb = visual.TextStim(window, text=replace_polish(config["pos_feedb"]), co
 neg_feedb = visual.TextStim(window, text=replace_polish(config["neg_feedb"]), color='black', height=25, pos=(0, -120))
 no_feedb = visual.TextStim(window, text=replace_polish(config["no_feedb"]), color='black', height=25, pos=(0, -120))
 feedb = {"pos": pos_feedb, "neg": neg_feedb, "no": no_feedb}
-
-break_time = visual.TextStim(window, text=replace_polish("Masz pół minuty przerwy. Nie odchodź od komputera"),
-                          color='black', height=config['TEXT_SIZE'], pos=(0, 0))
 
 
 # TRAINING
@@ -96,10 +97,10 @@ while mean_acc < config["min_training_acc"]:
     for info in data_train:
         idx_info = visual.TextStim(window, color='black', pos=(500, 400), height=50,
                                    text=i)
-        answers, rt, acc, A_to_B_relation = trial(window, config, answers_colors, info, mouse,
-                                 clock_image, feedb, mouse_info, idx_info)
+        answers, rt, acc, A_to_B_relation = trial(window, config, answers_colors, info, mouse, clock_image,
+                                                  feedb, mouse_info, idx_info)
 
-        RESULTS.append(prepare_result(i, info, answers, rt, acc, "train", A_to_B_relation))
+        RESULTS.append(prepare_result(i, info, answers, rt, acc, "train"))
         i += 1
         mean_acc += 1 if acc["left"] and acc["right"] else 0
     if i > 1:
@@ -118,20 +119,17 @@ show_info(window, join('.', 'messages', "instruction2.txt"), text_size=config['T
           screen_width=SCREEN_RES[0], key=config["exit_key"])
 
 i = 1
-for c, block in enumerate(data_exp):
-    for info in block:
-        idx_info = visual.TextStim(window, color='black', pos=(500, 400), height=50,
-                                   text=i)
-        answers, rt, acc, A_to_B_relation = trial(window, config, answers_colors, info,
-                                 mouse, clock_image, feedb, mouse_info, idx_info)
-        RESULTS.append(prepare_result(i, info, answers, rt, acc, "exp", A_to_B_relation))
-        i += 1
+for c in data_exp:
+    idx_info = visual.TextStim(window, color='black', pos=(500, 400), height=50, text=i)
+    answers, rt, acc, A_to_B_relation = trial(window, config, answers_colors, info,
+                                              mouse, clock_image, feedb, mouse_info, idx_info)
+    RESULTS.append(prepare_result(i, info, answers, rt, acc, "exp"))
+    i += 1
 
-        if i == config['break_after_n_trials']:
-            show_info(window, join('.', 'messages', "break.txt"), text_size=config['TEXT_SIZE'] + 25,
-                      screen_width=SCREEN_RES[0], key=config["exit_key"], color='green')
+    if i == config['break_after_n_trials']:
+        show_info(window, join('.', 'messages', "break.txt"), text_size=config['TEXT_SIZE'] + 25,
+                  screen_width=SCREEN_RES[0], key=config["exit_key"], color='green')
 
-    if c + 1 == 3:
-        show_info(window, join('.', 'messages', "end.txt"), text_size=config['TEXT_SIZE'], screen_width=SCREEN_RES[0])
+    show_info(window, join('.', 'messages', "end.txt"), text_size=config['TEXT_SIZE'], screen_width=SCREEN_RES[0])
 
 
