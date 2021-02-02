ba9f87a2b26d2d258d1c8fe04c09109419b24d06
Jan <jastrz.jan@gmail.com>
2021-02-02 18:12:00 +0100

    merge files

---
+++ results/merge_files.R
@@ -0,0 +1,19 @@
+if (!require('dplyr')) install.packages('dplyr')
+library(dplyr)
+
+files = list.files(pattern=".csv") 
+
+all_files = data.frame()
+for (file in files) {
+  dat = read.table(file, header=TRUE, sep=",")
+  all_files = bind_rows(all_files, dat) 
+}
+
+write.csv2(all_files, "MERGED_RESULTS.csv") # name of the file
+
+
+
+
+
+
+
