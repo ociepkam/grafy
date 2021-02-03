fae88aee0010f84b4baf880c62ad85359ef7ca62
Jan <jastrz.jan@gmail.com>
2021-02-03 16:54:51 +0100

    merge

--- results/merge_results.py
+++ results/merge_results.py
@@ -5,13 +5,13 @@ import glob
 
 def read(f):
     data = pd.read_csv(f, sep=";")
-    data["NAME"] = [f.split("/")[-1]]*len(data)
+    data["FILE_NAME"] = [f.split("/")[-1]]*len(data)
     return data
 
 
 def all_to_one_file(files_path, file_name):
     df = pd.concat(map(read, glob.glob(os.path.join(files_path, "*.csv"))))
-    df.to_csv(file_name + ".csv")
+    df.to_csv(file_name + ".csv", sep=";")
 
 
 all_to_one_file('raw_results', 'raw_results')
